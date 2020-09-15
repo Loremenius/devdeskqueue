fbf7e613e2abed11ad8ce946f918fc1c6dd0305a
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-09-15 00:34:12 -0600

    added form to login component. form has text and password inputs and a button

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="login">
         <h1>Login</h1>
+        <form class='login'>
+            <input type="text"/>
+            <input type="password"/>
+            <button>Login</button>
+        </form>
     </div>
 </template>
 
