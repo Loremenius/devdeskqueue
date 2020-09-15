df6678df2db51e3020b3b352ea9256f42c438008
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-09-14 19:10:37 -0600

    reformatted router to check for authorization if route requests it

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Dashboard from '../components/Dashboard'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -13,3 +13,31 @@ export default new Router({
     }
   ]
 })
+
+router.beforeEach((to, from, next)=>{
+  // if route needs authorization to access
+  if(to.matched.some(record => record.meta.requiresAuth)){
+    // if there is no token in local storage
+    if(localStorage.getItem('token') == null){
+      next({
+        path:'/login',
+        params:{ nextUrl: to.fullPath }
+      })
+    // if there is a token in local storage
+    } else {
+      // if the route needs admin to access
+      if(to.matched.some(record => record.meta.is_admin)){
+        // check if user is an admin
+
+      // if there is no need for admin access
+      } else {
+        next()
+      }
+    }
+  // if no authorization is needed
+  } else {
+    next()
+  }
+})
+
+export default router
