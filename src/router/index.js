a26eb8aced89efee3295139bedaf58515f644a61
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-09-14 19:22:54 -0600

    imported login to router. added meta isAuth to Dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '../components/Dashboard'
+import Login from '../components/Login'
 
 Vue.use(Router)
 
@@ -9,7 +10,15 @@ const router = new Router({
     {
       path: '/',
       name: 'Dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta:{
+        requiresAuth: true
+      }
+    },
+    {
+      path:'/login',
+      name:'Login',
+      component:Login
     }
   ]
 })
