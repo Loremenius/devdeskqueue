96b0c60a74a5e8a63e2b5ab50aaec43c52333248
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-09-16 15:52:07 -0600

    added form to register component

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -3,15 +3,31 @@
         <form class="registerForm">
             <label>
                 Username:
-                <input type="text" />
+                <input type="text" v-model.trim="username" />
             </label>
+            <label>
+                Password:
+                <input type="password" v-model.trim="password" />
+            </label>
+            <label>
+                Re-Enter Password:
+                <input type="password" v-model.trim="passAgain"/>
+            </label>
+            <button>Create Profile</button>
         </form>
     </div>
 </template>
 
 <script>
 export default {
-    
+    name:"Register",
+    data(){
+        return {
+            username:'',
+            password:'',
+            passAgain:''
+        }
+    }
 }
 </script>
 
