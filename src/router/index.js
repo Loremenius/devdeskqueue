384d4878dab52df9eabda2a620ba69753061c3a9
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-09-16 16:12:05 -0600

    added Register component to router

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '../components/Dashboard'
 import Login from '../components/Login'
+import Register from "../components/Register"
 
 Vue.use(Router)
 
@@ -19,6 +20,11 @@ const router = new Router({
       path:'/login',
       name:'Login',
       component:Login
+    },
+    {
+      path:'/register',
+      name:'Register',
+      component:Register
     }
   ]
 })
